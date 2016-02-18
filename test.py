d4098a24d9be29d036008c5ce48a0308a4a583d0
Kasper Koho <kasper.koho@taitounited.fi>
2016-02-18 18:40:47 +0200

    Alternate data source

--- test.py
+++ test.py
@@ -1,28 +1,40 @@
+from distutils import spawn
 import subprocess
 import re
-
-airodump = subprocess.Popen(
-    [
-        'airodump-ng', 'wlan1mon',
-        '--bssid', 'D8:84:66:42:BC:58',
-        '--channel', '13'
-    ],
-    stderr=subprocess.PIPE
-)
+import sys
 
 escape_chars = ['[2J', '[?25l', '[2J', '[J', chr(27), '[0m', '[1;1H']
+aircrack_installed = spawn.find_executable('airodump-ng') is not None
+if aircrack_installed:
+    airodump = subprocess.Popen(
+        [
+            'airodump-ng', 'wlan1mon',
+            '--bssid', 'D8:84:66:42:BC:58',
+            '--channel', '13'
+        ],
+        stderr=subprocess.PIPE
+    )
+
+    source = airodump.stderr
+else:
+    source = open('data.txt', 'r')
+
 while True:
-    output = airodump.stderr.readline()
-    if output == '' and airodump.poll() is not None:
+    output = source.readline()
+    if output == '' and aircrack_installed and airodump.poll() is not None:
         break
+
     if output:
         stripped = output.strip()
         for char in escape_chars:
             stripped = stripped.replace(char, '')
-        if (not stripped
-                or stripped.startswith('BSSID')
-                or stripped.startswith('CH 13')
-                or stripped.endswith('TaitoUnited')):
+
+        if (
+            not stripped or
+            stripped.startswith('BSSID') or
+            stripped.startswith('CH 13') or
+            stripped.endswith('TaitoUnited')
+        ):
             continue
 
         data = re.findall(
@@ -31,5 +43,5 @@ while True:
             '+-(?P<power>[0-9]+)',
             stripped
         )
+
         print(data)
-    rc = airodump.poll()
