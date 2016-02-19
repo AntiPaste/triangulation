679dcae9ef56372fd76610f7abd457306d9bb28a
Kasper Koho <antipaste@gmail.com>
2016-02-19 14:00:41 +0000

    Fixes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .coordinates import Coordinates  # noqa
+from .triangulator import Triangulator  # noqa
 from .datapoint import DataPoint  # noqa
 from .connection import Connection  # noqa
 from .master import Master  # noqa
